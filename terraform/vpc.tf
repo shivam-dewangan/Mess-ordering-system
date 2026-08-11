41d9ea78d7c612ee4239bf9f69206e2e460c5b5b
Shivam Dewangan <161064463+shivam-dewangan@users.noreply.github.com>
2026-08-11 18:37:00 +0530

    Add VPC module configuration in vpc.tf

---
+++ terraform/vpc.tf
@@ -0,0 +1,40 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "6.5.1"
+
+  name = "${var.project_name}-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs = [
+    "ap-south-1a",
+    "ap-south-1b"
+  ]
+
+  private_subnets = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ]
+
+  public_subnets = [
+    "10.0.101.0/24",
+    "10.0.102.0/24"
+  ]
+
+  enable_nat_gateway = true
+  single_nat_gateway  = true
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+
+  tags = {
+    Project = var.project_name
+  }
+}
