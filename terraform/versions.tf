989eef9fdc4a8a553fb9405a561edb2aca0f7512
Shivam Dewangan <161064463+shivam-dewangan@users.noreply.github.com>
2026-08-11 18:40:28 +0530

    Update Terraform version and add providers
    Updated required Terraform version and added new providers.

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,10 +1,25 @@
 terraform {
-  required_version = ">= 1.9.0"
+  required_version = ">= 1.5.7"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "6.55.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.38.0"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "3.2.0"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.1.0"
     }
   }
 }
